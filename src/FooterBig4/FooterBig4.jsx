5b5e3d2e7bafb8017f13491f6d4204121d6757d4
jszmidla <xurexpl@gmail.com>
2024-12-12 01:36:18 +0100

    changed a little to bootstrap

--- src/FooterBig4/FooterBig4.jsx
+++ src/FooterBig4/FooterBig4.jsx
@@ -1,74 +1,29 @@
 import "./FooterBig4.css";
-import { Logo } from "../Logo/Logo.jsx";
-import { SocialIconsStyleWhiteSocialInstagram } from "../SocialIconsStyleWhiteSocialInstagram/SocialIconsStyleWhiteSocialInstagram.jsx";
-import { SocialIconsStyleWhiteSocialDribbble } from "../SocialIconsStyleWhiteSocialDribbble/SocialIconsStyleWhiteSocialDribbble.jsx";
-import { SocialIconsStyleWhiteSocialTwitter } from "../SocialIconsStyleWhiteSocialTwitter/SocialIconsStyleWhiteSocialTwitter.jsx";
-import { SocialIconsStyleWhiteSocialYoutube } from "../SocialIconsStyleWhiteSocialYoutube/SocialIconsStyleWhiteSocialYoutube.jsx";
-import { EssentialIconsSend } from "../EssentialIconsSend/EssentialIconsSend.jsx";
 
 export const FooterBig4 = ({ className, ...props }) => {
   return (
-    <div className={"footer-big-4 " + className}>
-      <div className="company-info">
-        <Logo className="logo-instance"></Logo>
-        <div className="copyright">
-          <div className="copyright-2020-landify-ui-kit">
-            Copyright © 2020 Landify UI Kit.{" "}
-          </div>
-          <div className="all-rights-reserved">All rights reserved </div>
-        </div>
-        <div className="social-links">
-          <SocialIconsStyleWhiteSocialInstagram
-            styleVariant="white"
-            social="instagram"
-            className="social-icons-instance"
-          ></SocialIconsStyleWhiteSocialInstagram>
-          <SocialIconsStyleWhiteSocialDribbble
-            styleVariant="white"
-            social="dribbble"
-            className="social-icons-instance2"
-          ></SocialIconsStyleWhiteSocialDribbble>
-          <SocialIconsStyleWhiteSocialTwitter
-            styleVariant="white"
-            className="social-icons-instance3"
-          ></SocialIconsStyleWhiteSocialTwitter>
-          <SocialIconsStyleWhiteSocialYoutube
-            styleVariant="white"
-            social="youtube"
-            className="social-icons-instance4"
-          ></SocialIconsStyleWhiteSocialYoutube>
+      <div className="footer-big-4 py-5">
+          <div className="container d-flex flex-wrap ">
+            <div className="col-md-6 mb-3 d-flex flex-column justify-content-center align-items-center">
+              <div>
+                <h1>Smart Parking</h1>
+                <div className="mt-2">
+                  <div>All rights reserved</div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6 mb-3 d-flex flex-column justify-content-center align-items-center">
+              <div>
+                <h5>Company</h5>
+                <ul className="list-unstyled mt-3">
+                  <li>
+                    <a href="#">About us</a>
+                  </li>
+                  <a href="https://smartparking.com.pl/contact-us">Contact us</a>
+                </ul>
+              </div>
+            </div>
         </div>
       </div>
-      <div className="links">
-        <div className="col-1">
-          <div className="company">Company </div>
-          <div className="list-items">
-            <div className="about-us">About us </div>
-            <div className="blog">Blog </div>
-            <div className="contact-us">Contact us </div>
-            <div className="pricing">Pricing </div>
-            <div className="testimonials">Testimonials </div>
-          </div>
-        </div>
-        <div className="col-2">
-          <div className="support">Support </div>
-          <div className="list-items">
-            <div className="help-center">Help center </div>
-            <div className="terms-of-service">Terms of service </div>
-            <div className="legal">Legal </div>
-            <div className="privacy-policy">Privacy policy </div>
-            <div className="status">Status </div>
-          </div>
-        </div>
-        <div className="col-3">
-          <div className="stay-up-to-date">Stay up to date </div>
-          <div className="email-input-field">
-            <div className="input-field-bg"></div>
-            <EssentialIconsSend className="essential-icons-send-instance"></EssentialIconsSend>
-            <div className="your-email-address">Your email address </div>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 };
--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -3,228 +3,176 @@ import { FooterBig4 } from "../FooterBig4/FooterBig4.jsx";
 
 export const LandingPage = ({ className, ...props }) => {
   return (
-    <div className={"landing-page " + className}>
-      <div className="home">
-        <div className="hero-section">
-          <div className="frame-1">
-            <div className="text">
-              <div className="unlock-parking-potential-with-smart-solution">
-                <span>
-                  <span className="unlock-parking-potential-with-smart-solution-span">
-                    Unlock Parking Potential <br />
-                  </span>
-                  <span className="unlock-parking-potential-with-smart-solution-span2">
-                    with Smart Solution
-                  </span>
-                </span>{" "}
-              </div>
-              <div className="do-you-own-a-parking-attract-more-customers-with-ease">
-                Do you own a parking? Attract more customers with ease!{" "}
-              </div>
-            </div>
-          </div>
-          <div className="illustration">
-            <img
-              className="image-ai-brush-removebg-6-ucjfkam-1"
-              src="image-ai-brush-removebg-6-ucjfkam-10.png"
-            />
-          </div>
-        </div>
-        <div className="clients">
-          <div className="clients2">
-            <div className="section-title">
-              Our app integrates seamlessly with any parking occupancy tracking
-              system.{" "}
-            </div>
-          </div>
-          <div className="clients-logos">
-            <div className="logo">
-              <div className="logo2">
-                <img
-                  className="parking-barrier-2-1"
-                  src="parking-barrier-2-10.png"
-                />
+        <div className="container-fluid landing-page">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+            <div className="container">
+              <a className="navbar-brand" href="#">
+                <img className="img-fluid" src="image-230.png" alt="Logo" />
+                <span className="text-primary">Smart</span>
+                <span className="text-secondary">Parking</span>
+              </a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="btn btn-primary" href="#">Sign up</a>
+                  </li>
+                </ul>
               </div>
             </div>
-            <div className="logo">
-              <div className="logo3">
-                <img className="image-22" src="image-220.png" />
-              </div>
-            </div>
-            <div className="logo">
-              <img className="image-21" src="image-210.png" />
-            </div>
-            <div className="logo">
-              <img
-                className="parking-barrier-2-12"
-                src="parking-barrier-2-11.png"
-              />
-            </div>
-            <div className="logo">
-              <img className="image-222" src="image-221.png" />
-            </div>
-          </div>
-        </div>
-        <div className="community">
-          <div className="community2">
-            <div className="section-title2">
-              Manage all of your car parks in one place.{" "}
-            </div>
-            <div className="description">
-              Who can benefit from Smart Parking?{" "}
-            </div>
-          </div>
-          <div className="frame-4">
-            <div className="membership-organizatios">
-              <div className="member">
-                <div className="icon">
-                  <div className="rectangle-157"></div>
-                  <img className="icon2" src="icon1.svg" />
-                </div>
-                <div className="private-owners">
-                  Private <br />
-                  owners{" "}
-                </div>
-              </div>
-            </div>
-            <div className="national-associations">
-              <div className="member">
-                <div className="icon">
-                  <div className="rectangle-157"></div>
-                  <div className="icon3">
-                    <img className="group-1" src="group-10.svg" />
+          </nav>
+            <div className="hero-section mt-5 py-5">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-6 text justify-content-center d-flex flex-column">
+                    <h1 className="display-4">
+                      Unlock Parking Potential <br />
+                      <small className="text-muted smart-solution">with Smart Solution</small>
+                    </h1>
+                    <p className="lead">Do you own a parking? Attract more customers with ease!</p>
+                  </div>
+                  <div className="col-md-6 illustration">
+                    <img
+                        className="img-fluid"
+                        src="image-ai-brush-removebg-6-ucjfkam-10.png"
+                        alt="Illustration"
+                    />
                   </div>
                 </div>
-                <div className="cities">Cities </div>
-              </div>
             </div>
-            <div className="national-associations">
-              <div className="member">
-                <div className="icon">
-                  <div className="rectangle-157"></div>
-                  <div className="icon3">
-                    <img className="group-2" src="group-20.svg" />
+          </div>
+          <div className="clients my-5 pt-5">
+            <div className="container">
+                <h2 className="text-center">Our app integrates seamlessly with any parking occupancy tracking system.</h2>
+                <div className="row text-center py-5">
+                  <div className="col">
+                    <img
+                        className="img-fluid"
+                        src="parking-barrier-2-10.png"
+                        alt="Client Logo"
+                    />
+                  </div>
+                  <div className="col">
+                    <img
+                        className="img-fluid"
+                        src="image-220.png"
+                        alt="Client Logo"
+                    />
+                  </div>
+                  <div className="col">
+                    <img
+                        className="img-fluid"
+                        src="image-210.png"
+                        alt="Client Logo"
+                    />
+                  </div>
+                  <div className="col">
+                    <img
+                        className="img-fluid"
+                        src="parking-barrier-2-11.png"
+                        alt="Client Logo"
+                    />
+                  </div>
+                  <div className="col">
+                    <img
+                        className="img-fluid"
+                        src="image-221.png"
+                        alt="Client Logo"
+                    />
                   </div>
                 </div>
-                <div className="institutions">Institutions </div>
+            </div>
+          </div>
+          <div className="community my-5">
+            <div className="container">
+              <h2 className="text-center">Manage all of your car parks in one place.</h2>
+              <p className="text-center mt-2">Who can benefit from Smart Parking?</p>
+              <div className="row text-center py-5">
+                <div className="col-md-4">
+                  <img className="img-fluid" src="icon1.svg" alt="Private Owners" />
+                  <p>Private owners</p>
+                </div>
+                <div className="col-md-4">
+                  <img className="img-fluid" src="group-10.svg" alt="Cities" />
+                  <p>Cities</p>
+                </div>
+                <div className="col-md-4">
+                  <img className="img-fluid" src="group-20.svg" alt="Institutions" />
+                  <p>Institutions</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="body">
-        <div className="achievements">
-          <div className="section-heading">
-            <div className="section-heading2">
-              <div className="section-title3">
-                <span>
-                  <span className="section-title-3-span">
+          <div className="achievements my-5 pt-5">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6 justify-content-center d-flex flex-column">
+                  <h2 className="text-center">
                     Helping parking owners <br />
-                  </span>
-                  <span className="section-title-3-span2">
-                    attract more customers!
-                  </span>
-                </span>{" "}
-              </div>
-            </div>
-          </div>
-          <div className="counts">
-            <div className="row-1">
-              <div className="_2">
-                <img className="icon4" src="icon6.svg" />
-                <div className="details">
-                  <div className="label">Members </div>
+                    <small className="text-muted">attract more customers!</small>
+                  </h2>
                 </div>
-              </div>
-              <div className="_1">
-                <div className="icon5">
-                  <img className="live-tv" src="live-tv0.svg" />
-                </div>
-                <div className="details">
-                  <div className="label">Real-Time Spot Updates </div>
-                </div>
-              </div>
-            </div>
-            <div className="row-2">
-              <div className="_3">
-                <img className="icon6" src="icon8.svg" />
-                <div className="details">
-                  <div className="label">Event Bookings </div>
-                </div>
-              </div>
-              <div className="_4">
-                <div className="icon7">
-                  <img className="smart-toy" src="smart-toy0.svg" />
-                </div>
-                <div className="details">
-                  <div className="label">Innovative AI Solution </div>
+
+                <div className="col-md-6">
+                  <div className="row text-center">
+                    <div className="col-md-6">
+                      <img className="img-fluid" src="icon6.svg" alt="Members" />
+                      <p>Members</p>
+                    </div>
+                    <div className="col-md-6">
+                      <img className="img-fluid" src="live-tv0.svg" alt="Real-Time Spot Updates" />
+                      <p>Real-Time Spot Updates</p>
+                    </div>
+                    <div className="col-md-6">
+                      <img className="img-fluid" src="icon8.svg" alt="Event Bookings" />
+                      <p>Event Bookings</p>
+                    </div>
+                    <div className="col-md-6">
+                      <img className="img-fluid" src="smart-toy0.svg" alt="Innovative AI Solution" />
+                      <p>Innovative AI Solution</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="calender">
-          <div className="unlock">
-            <div className="_2203-i-402-026-s-m-004-c-13-parking-flat-composition-1">
-              <img className="g-8" src="g-80.svg" />
-            </div>
-            <div className="frame-6">
-              <div className="frame-5">
-                <div className="how-our-system-works">
-                  How our system works?{" "}
+          <div className="calender my-5">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-6 illustration">
+                  <img
+                      className="img-fluid"
+                      src="image-ai-brush-removebg-6-ucjfkam-10.png"
+                      alt="Illustration"
+                  />
                 </div>
-                <div className="one-event-calendar-shared-across-national-regional-and-club-events-to-simplify-your-event-experience-allowing-you-to-create-waiting-lists-share-data-capture-issue-credential-for-course-completion-apply-smart-purchasing-rules-and-much-more">
-                  Our parking management system offers seamless integration with
-                  existing gate systems or sensor setups through API
-                  connectivity. This means you can connect your current parking
-                  infrastructure directly to our app, allowing for efficient,
-                  centralized control and monitoring.
-                  <br />
-                  In addition, our solution includes advanced AI-driven camera
-                  technology for counting available spaces. Using computer
-                  vision, our model analyzes live video feeds to accurately
-                  detect and report occupancy in real time. This enables a more
-                  flexible and scalable approach to parking management,
-                  providing your customers with reliable, up-to-date information
-                  on space availability.{" "}
+                <div className="col-md-6 text justify-content-center d-flex flex-column">
+                  <h2 className="text-center">How our system works?</h2>
+                  <p className="text-center mt-3">
+                    Our parking management system offers seamless integration with existing gate systems or sensor setups through API connectivity. This means you can connect your current parking infrastructure directly to our app, allowing for efficient, centralized control and monitoring.
+                    <br />
+                    In addition, our solution includes advanced AI-driven camera technology for counting available spaces. Using computer vision, our model analyzes live video feeds to accurately detect and report occupancy in real time. This enables a more flexible and scalable approach to parking management, providing your customers with reliable, up-to-date information on space availability.
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="frame-16">
-          <div className="let-nextcent-do-the-work-so-you-don-t-have-to">
-            Leave It to the Pros: Simplify Parking Management in One Platform!{" "}
+          <div className="footer py-5">
+            <div className="py-4">
+              <div className="container">
+                <h2 className="text-center">Leave It to the Pros: Simplify Parking Management in One Platform!</h2>
+              </div>
+            </div>
           </div>
+          <FooterBig4 className="footer-big-4-instance"></FooterBig4>
         </div>
-        <FooterBig4 className="footer-big-4-instance"></FooterBig4>
-      </div>
-      <div className="frame-36">
-        <div className="heading-name">
-          <div className="header-menu-default">
-            <div className="label2">Home </div>
-          </div>
-          <div className="menu-item-default">
-            <div className="label3">Contact </div>
-          </div>
-        </div>
-        <div className="login">
-          <div className="button">
-            <div className="sign-up">Sign up </div>
-          </div>
-        </div>
-        <img className="image-23" src="image-230.png" />
-        <div className="logo4">
-          <div className="smartparking">
-            <span>
-              <span className="smartparking-span">Smart</span>
-              <span className="smartparking-span2">Parking</span>
-            </span>{" "}
-          </div>
-        </div>
-      </div>
-    </div>
   );
 };
